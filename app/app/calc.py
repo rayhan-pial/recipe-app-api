33531dd8d5cdd9b9923b78ff7eca572c763a470c
Rayhan <pialshek08@gmail.com>
2023-06-21 02:45:43 +0600

    Done ading phycopg2 10th time

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,10 +3,11 @@ Calculator functions
 """
 
 
-def add(x,y):
+def add(x, y):
+
+    return x + y
+
+
+def subtract(x, y):
     
-    return x+y
-
-def subtract(x,y):
-
-    return y-x
+    return y - x
