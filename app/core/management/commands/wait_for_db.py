fd8a4789e11bfaa7f4a192abc0619de009e64903
Rayhan <pialshek08@gmail.com>
2023-07-05 22:32:20 +0600

    Configured docker compose and checks to wait for db 3

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -24,5 +24,5 @@ class Command(BaseCommand):
             except (Psycopg20Error, OperationalError):
                 self.stdout.write('Database unavailable, wait for 1 sec....')
                 time.sleep(1)
-        
+
         self.stdout.write(self.style.SUCCESS('Database available'))
